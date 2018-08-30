f742200b2d5315b6284f37fddcf495b9140301c7
William <william.lafontaine@infobec.ca>
2018-08-30 14:21:00 -0400

    ClassBd

--- src/testbd/Interface.java
+++ src/testbd/Interface.java
@@ -5,9 +5,6 @@
  */
 package testbd;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -15,60 +12,11 @@ import javax.swing.table.DefaultTableModel;
  */
 public class Interface extends javax.swing.JFrame {
 
-    java.sql.Connection conn;
     /**
      * Creates new form Interface
      */
     public Interface() {
         initComponents();
-        
-        //Server
-        String driver = "com.mysql.cj.jdbc.Driver";
-        String servername = "localhost";
-        String port = "3306";
-        String shema = "test_shema";
-        String parameter = "?serverTimezone=UTC";
-        String url = "jdbc:mysql://" + servername + ":" + port + "/" + shema + parameter;
-        String username = "root";
-        String password = "t0t0g5wil"; 
-        
-        try{
-            Class.forName(driver).newInstance();
-            conn = java.sql.DriverManager.getConnection(
-            url, username, password);
-            } catch(Exception e){
-            System.out.println("Connection ratée: "+e);
-            System.exit(-1);
-        }
-        
-        //ListItems
-        
-        PreparedStatement pst=null;
-        ResultSet rs = null;
-        String Requete = "SELECT * FROM new_table";
-        try
-        {
-        pst = conn.prepareStatement(Requete, 1005, 1008);
-        //1005 == ResultSet.TYPE_SCROLL_SENSITIVE
-        //1008 == ResultSet.CONCUR_UPDATABLE
-        pst.clearParameters();
-        rs = pst.executeQuery();
-        
-        String header[] = new String[] { "id", "one", "two",
-            "three", "four", "five" };
-        DefaultTableModel dtm = new DefaultTableModel(0, 0);
-        dtm.setColumnIdentifiers(header);
-        ListItems.setModel(dtm);
-        while(rs.next()){
-            dtm.addRow(new Object[] { rs.getString(0), rs.getString(1), rs.getString(2),
-                             rs.getString(3), rs.getString(4) });
-        }
-        //ListItems.setListData();
-        }
-        catch(Exception e){
-            System.out.println("Connection ratée: "+e);
-            System.exit(-1);
-        }
     }
 
     /**
