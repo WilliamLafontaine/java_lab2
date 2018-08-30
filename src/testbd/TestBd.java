34cf6daa759bbf9ad87e859bc34915a1d473bd27
William <william.lafontaine@infobec.ca>
2018-08-30 11:46:29 -0400

    init

---
+++ src/testbd/TestBd.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testbd;
+
+import java.sql.*;
+/**
+ *
+ * @author wil90
+ */
+public class TestBd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interface frm = new Interface();
+        frm.setVisible(true);
+    }
+
+}
